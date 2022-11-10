f8e2246bd028d3d5119beb29f6a2337ee7b912cf
M4rteaga <m4rteagag@gmail.com>
2022-11-09 22:43:13 -0500

    add price and image to variants

--- components/CRUDProductsBusiness/FormAddProduct.tsx
+++ components/CRUDProductsBusiness/FormAddProduct.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from 'next/image';
+import { type } from 'os';
 import React, { useRef, useState } from 'react';
 import {
 	Control,
@@ -7,7 +8,7 @@ import {
 	UseFormRegister,
 	UseFormRegisterReturn,
 } from 'react-hook-form';
-import { generateVariants } from '../../lib/products';
+import { generateVariants, ProductsGenerated } from '../../lib/products';
 import { GenericInput } from '../genericInput';
 
 type FormData = {
@@ -19,7 +20,7 @@ type FormData = {
 		options: string;
 	}[];
 	image?: FileList;
-	precio?: number;
+	price?: number;
 };
 
 interface SectionsProps {
@@ -61,7 +62,7 @@ const NoVariantsSection = ({ register }: SectionsProps) => {
 			</div>
 			<GenericInput
 				label="Precio:"
-				inputProps={register('precio', { required: true })}
+				inputProps={register('price', { required: true })}
 			/>
 			<button type="submit">Continuar</button>
 		</section>
@@ -140,8 +141,83 @@ const VariantsSection = ({ control, register }: SectionsProps) => {
 	);
 };
 
+export interface Product {
+	name: string;
+	description: string;
+	characteristics: {
+		type: string;
+		value: string;
+	}[];
+	image: FileList;
+	price: number;
+}
+interface ProductsVariants {
+	products: {
+		image: FileList;
+		price: number;
+	}[];
+}
+
+const GeneratedVariantsSection = ({
+	variants,
+}: {
+	variants: ProductsGenerated[];
+}) => {
+	const {
+		register,
+		control,
+		setValue,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<ProductsVariants>();
+
+	const { fields, append, remove } = useFieldArray({
+		name: 'products',
+		control,
+	});
+
+	const onSubmit = handleSubmit((data) => {
+		const products = [];
+		for (let i = 0; i < variants.length; i++) {
+			const p: Product = {
+				...variants[i],
+				image: data.products[i].image,
+				price: data.products[i].price,
+			};
+			products.push(p);
+		}
+		console.log(products);
+	});
+
+	const PlaceholderName = ({ n }: { n: string }) => <p>{n.split(' ').pop()}</p>;
+
+	return (
+		<section className="flex flex-col w-1/2 mx-auto px-5 py-6 mt-8 rounded-md gap-8 bg-purple-200 border-purple-400 border-2 ">
+			<form onSubmit={onSubmit} className="flex flex-col space-y-4">
+				{variants.map((v, key) => (
+					<div className="flex flex-row space-x-3" key={key}>
+						<PlaceholderName n={v.name} />
+						<input
+							type={'number'}
+							{...register(`products.${key}.price`, { required: true })}
+						/>
+						<input
+							type="file"
+							accept="image/*"
+							{...register(`products.${key}.image`, { required: true })}
+						/>
+					</div>
+				))}
+				<button>create products</button>
+			</form>
+		</section>
+	);
+};
+
 const FormAddProduct = () => {
 	const [hasVariants, setHasVariants] = useState<boolean | null>(null);
+	const [generating, setGenerating] = useState<boolean>(false);
+	const [variants, setVariants] = useState<ProductsGenerated[]>();
 	const {
 		register,
 		control,
@@ -170,72 +246,83 @@ const FormAddProduct = () => {
 
 	const onSubmit = handleSubmit((data) => {
 		if (data.hasVariants === 'true') {
-			generateVariants(data);
+			setGenerating(true);
+			const v = generateVariants(data);
+			setVariants(v);
+			setGenerating(false);
 		}
 	});
 
 	return (
 		<div className="flex flex-col items-center">
 			<h1 className="text-3xl">Agregar producto a catalogo</h1>
-			<form onSubmit={onSubmit} className="flex flex-col w-full">
-				<section className="flex flex-col w-1/2 mx-auto px-5 py-6 mt-8 rounded-md gap-8 bg-purple-200 border-purple-400 border-2 ">
-					<h4 className="text-center text-lg font-bold">Primera etapa</h4>
-					<GenericInput
-						label="Nombre:"
-						inputProps={register('name', { required: true })}
-					/>
-					<div className="flex flex-col justify-start ">
-						<label className="text-gray-700">Descripción:</label>
-						<textarea
-							{...register('description', { required: true })}
-							className="mr-8 p-1 rounded-md w-full mt-2"
+			{generating ? <p>loading</p> : null}
+			{!variants ? (
+				<form onSubmit={onSubmit} className="flex flex-col w-full">
+					<section className="flex flex-col w-1/2 mx-auto px-5 py-6 mt-8 rounded-md gap-8 bg-purple-200 border-purple-400 border-2 ">
+						<h4 className="text-center text-lg font-bold">Primera etapa</h4>
+						<GenericInput
+							label="Nombre:"
+							inputProps={register('name', { required: true })}
 						/>
-					</div>
-					<section aria-label="variantes" className="flex flex-col items-start">
-						<p>Variants:</p>
-						<div className="flex flex-row gap-4">
-							<div className="mt-2 flex flex-col justify-center">
-								<input
-									{...radioRegister}
-									type="radio"
-									value="true"
-									onChange={(v) => onchangeHasVariants(v)}
-								/>
-								<label htmlFor="">Si</label>
-							</div>
-							<div className="mt-2 flex flex-col justify-center">
-								<input
-									{...radioRegister}
-									type="radio"
-									value="false"
-									onChange={(v) => onchangeHasVariants(v)}
-								/>
-								<label htmlFor="">No</label>
-							</div>
+						<div className="flex flex-col justify-start ">
+							<label className="text-gray-700">Descripción:</label>
+							<textarea
+								{...register('description', { required: true })}
+								className="mr-8 p-1 rounded-md w-full mt-2"
+							/>
 						</div>
+						<section
+							aria-label="variantes"
+							className="flex flex-col items-start"
+						>
+							<p>Variants:</p>
+							<div className="flex flex-row gap-4">
+								<div className="mt-2 flex flex-col justify-center">
+									<input
+										{...radioRegister}
+										type="radio"
+										value="true"
+										onChange={(v) => onchangeHasVariants(v)}
+									/>
+									<label htmlFor="">Si</label>
+								</div>
+								<div className="mt-2 flex flex-col justify-center">
+									<input
+										{...radioRegister}
+										type="radio"
+										value="false"
+										onChange={(v) => onchangeHasVariants(v)}
+									/>
+									<label htmlFor="">No</label>
+								</div>
+							</div>
+						</section>
 					</section>
-				</section>
-				{hasVariants == true ? (
-					<VariantsSection register={register} control={control} />
-				) : hasVariants == false ? (
-					<NoVariantsSection register={register} />
-				) : null}
-				{hasVariants == true ? (
-					<button
-						className=" mt-5 mx-auto py-1 px-3 rounded-md bg-purple-700 text-white"
-						type="submit"
-					>
-						Generar variantes
-					</button>
-				) : hasVariants == false ? (
-					<button
-						className=" mt-5 mx-auto py-1 px-3 rounded-md bg-purple-700 text-white"
-						type="submit"
-					>
-						Guardar producto
-					</button>
-				) : null}
-			</form>
+					{hasVariants == true ? (
+						<VariantsSection register={register} control={control} />
+					) : hasVariants == false ? (
+						<NoVariantsSection register={register} />
+					) : null}
+					{hasVariants == true ? (
+						<button
+							className=" mt-5 mx-auto py-1 px-3 rounded-md bg-purple-700 text-white"
+							type="submit"
+						>
+							Generar variantes
+						</button>
+					) : hasVariants == false ? (
+						<button
+							className=" mt-5 mx-auto py-1 px-3 rounded-md bg-purple-700 text-white"
+							type="submit"
+						>
+							Guardar producto
+						</button>
+					) : null}
+				</form>
+			) : (
+				<GeneratedVariantsSection variants={variants} />
+			)}
 		</div>
 	);
 };
