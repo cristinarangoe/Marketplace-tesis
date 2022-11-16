56230045081a35fdce74a6d012335cacdcfc166a
M4rteaga <m4rteagag@gmail.com>
2022-11-16 02:17:44 -0500

    marketplace to lowercase

---
+++ pages/marketplace/BusinessesPage.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import useSWR from 'swr';
+import BusinessItem from '../../components/MarketplaceBusiness/BusinessItem';
+import Loading from '../../components/Navbar-Navigation/Loading';
+import Navbar from '../../components/Navbar-Navigation/Navbar';
+import { CLIENT_URL } from '../../lib/client';
+import fetcher from '../../lib/utils';
+import { BusinessInMarketplace, Item } from '../../types';
+
+function BusinessesPage() {
+	const { data, error } = useSWR<BusinessInMarketplace[], Error>(
+		`${CLIENT_URL}/business`,
+		fetcher
+	);
+
+	if (error)
+		return (
+			<div>
+				<p>error</p>
+			</div>
+		);
+
+	if (!data) return <Loading />;
+
+	let businesses: BusinessInMarketplace[] = data;
+	return (
+		<div>
+			<Navbar />
+			<div className="mt-5 mx-10">
+				<div className="text-center">
+					<h1 className="text-3xl font-bold">
+						Emprendimientos que hacen parte de nuestra plataforma!
+					</h1>
+					<p className="mt-3 text-lg font-semibold">
+						Presiona sobre cada uno de ellos para que conozcas lo que son y sus
+						productos
+					</p>
+				</div>
+				<div className="grid grid-cols-5 gap-x-4 gap-y-8 mt-8">
+					{businesses.map((bus, index) => (
+						<BusinessItem business={bus} key={index} />
+					))}
+				</div>
+				<div></div>
+			</div>
+		</div>
+	);
+}
+
+export default BusinessesPage;
---
+++ pages/marketplace/user/[id]/orders/index.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from 'react';
+import OrderItemUser from '../../../../../components/UserProfileMarketplace/OrderItemUser';
+import UserLayout from '../../../../../layouts/UserLayout';
+import useSWR from 'swr';
+import { OrderDB } from '../../../../../types';
+import { NextPageWithLayout } from '../../../../_app';
+import { CLIENT_URL } from '../../../../../lib/client';
+import fetcher from '../../../../../lib/utils';
+import { userSignal } from '../../../../../signals/userSignal';
+import Loading from '../../../../../components/Navbar-Navigation/Loading';
+
+const UserOrders: NextPageWithLayout = () => {
+	const { data, error } = useSWR<OrderDB[], Error>(
+		`${CLIENT_URL}/orders/${userSignal.value?.data._id}`,
+		fetcher
+	);
+
+	if (error)
+		return (
+			<div>
+				<p>error</p>
+			</div>
+		);
+
+	if (!data) return <Loading />;
+
+	return (
+		<>
+			<div className="mb-2">
+				<h1 className="text-3xl font-bold">Órdenes</h1>
+			</div>
+			<div className="flex flex-row items-center border-b-2 border-b-gray-100 mt-5 pb-2">
+				<div className="basis-1/2 mx-auto">
+					<h3 className="text-lg font-medium text-medium-violet">
+						Número de orden
+					</h3>
+				</div>
+				<div className="basis-1/2">
+					<h3 className="text-lg font-medium text-medium-violet">Total</h3>
+				</div>
+			</div>
+			<div>
+				{data.map((order, index) => (
+					<OrderItemUser key={index} order={order} />
+				))}
+			</div>
+		</>
+	);
+};
+
+UserOrders.getLayout = function getLayout(page: ReactElement) {
+	return <UserLayout>{page}</UserLayout>;
+};
+
+export default UserOrders;
